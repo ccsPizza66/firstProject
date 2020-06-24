7e55563005064eed27c29225b32c5a6bbb7823da
CC <chuanchin66@hotmail.com>
2020-06-24 00:12:41 -0500

    make stuff public

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/sounds'));
 app.use(express.static(__dirname + '/svg'));
 app.use(express.static(__dirname + '/video'));
 
 //app.get('/', (req, res) => res.send('psassa'));
-app.get('/', (req, res) => res.sendFile(__dirname + 'index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '\\index.html'));
 
 app.listen(PORT, () => console.log(`Listening on 5000 mofos`));
